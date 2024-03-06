afe46e885c5d14d8e2e1843d77e1ab591ebd7688
CristiniLorenzo <lorenzo.cristini98@gmail.com>
2024-03-06 16:24:03 +0100

    create html, css, js file. build the html structure whith css style

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Scrivere un programma che chieda all’utente:
+// - Il numero di chilometri da percorrere
+// - Età del passeggero
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// - va applicato uno sconto del 20% per i minorenni
+// - va applicato uno sconto del 40% per gli over 65.
+const generateButton = document.querySelector('#generate-button');
+generateButton.addEventListener('click', function({
+    // leggo i valori degli input
+    const userName = document.querySelector('#user-name').value;
+    const userAge = document.querySelector('#user-age').value;
+    console.log(userName);
+    // calcolo del prezzo base 
+
+}))
